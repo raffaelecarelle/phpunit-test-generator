f61b0697b46a29e2f34bfa3dec14aba7a6de7cf5
Raffaele Carele <raffaele.carelle@algoritma.it>
2024-10-28 22:31:21 +0100

    fix cs

--- tests/TestClassMetadataParserTest.php
+++ tests/TestClassMetadataParserTest.php
@@ -1,31 +1,29 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
 
 namespace JWage\PHPUnitTestGenerator\Tests;
 
-use JWage\PHPUnitTestGenerator\Tests\Fixture\TestClass1;
 use Doctrine\Inflector\Inflector;
 use JWage\PHPUnitTestGenerator\TestClassMetadata;
 use JWage\PHPUnitTestGenerator\TestClassMetadataParser;
+use JWage\PHPUnitTestGenerator\Tests\Fixture\TestClass1;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class TestClassMetadataParserTest extends TestCase
 {
-    /** @var Inflector|MockObject */
     private Inflector $inflector;
 
-    /** @var TestClassMetadataParser */
     private TestClassMetadataParser $testClassMetadataParser;
 
     public function testGetTestClassMetadata(): void
     {
         $className = TestClass1::class;
         self::assertInstanceOf(TestClassMetadata::class, $this->testClassMetadataParser->getTestClassMetadata($className));
-
     }
 
+    #[\Override]
     protected function setUp(): void
     {
         $this->inflector = $this->createMock(Inflector::class);
