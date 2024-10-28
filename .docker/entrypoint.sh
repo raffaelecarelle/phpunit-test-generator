40d81d557092e07c47916ef360cee811e6fec8fd
Raffaele Carele <raffaele.carelle@algoritma.it>
2024-10-28 22:20:11 +0100

    rename docker user

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,9 +6,9 @@ DOCKER_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 COMPOSER_DIR=~/.composer
 mkdir -p $COMPOSER_DIR
 
-docker build -t testforge $DOCKER_DIR
+docker build -t application $DOCKER_DIR
 docker run --rm -ti \
-  -v $DOCKER_DIR/../:/home/testforge/projects \
-  -v $COMPOSER_DIR:/home/testforge/.composer \
-  -u testforge \
-  testforge zsh
+  -v $DOCKER_DIR/../:/home/application/projects \
+  -v $COMPOSER_DIR:/home/application/.composer \
+  -u application \
+  application zsh
